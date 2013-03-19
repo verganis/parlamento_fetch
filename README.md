68094cf0a397c349a06ec9e4f7f3d3ed475e3c2b
verganis <stefano.vergani.it@gmail.com>
2013-03-19 15:11:20 +0100

    updated readme

---
+++ README.md
@@ -0,0 +1,4 @@
+Parlamento fetch
+----------------
+
+These scripts fetch data from Camera dei deputati and Senato della Repubblica SPARQL endpoint to update Open Parlamento database.
---
+++ parlamento_fetch/senato/s_senatori_composizione_d.py
@@ -0,0 +1,2 @@
+# a partire da due date verifica le differenze fra i senatori in carica a data1 e data2 e salva due file
+# AAAA-MM-GG-senatori_added e AAAA-MM-GG-senatori_removed con le d
