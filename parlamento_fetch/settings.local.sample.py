25a230dc1c573eb25fd9dc9c68f6a74291d4fd38
Depp <stefano.vergani.it@gmail.com>
2014-03-12 12:39:47 +0100

    Added sample local settings

---
+++ parlamento_fetch/settings.local.sample.py
@@ -0,0 +1,14 @@
+from settings import *
+
+notification_list=[
+    "NOTIFICATION-MAIL@GMAIL.COM",
+]
+
+smtp_server='HOST'
+
+# Google Account credentials
+g_user = 'USER'
+g_password = 'PASSW'
+
+vne_list_address = 'https://docs.google.com/spreadsheet/ccc?key=---INSERT-GDOC-KEY-HERE---&single=true&gid=0&output=csv'
+
